c8d925c19ba3958b066f1a34f77cab863c5171bd
sabbir <mdsabbir71a@gmail.com>
2026-09-25 21:04:57 +0600

    Fix native Google credential exception import

--- app/src/main/java/com/focuslock/app/AuthActivity.java
+++ app/src/main/java/com/focuslock/app/AuthActivity.java
@@ -39,8 +39,8 @@ import androidx.credentials.Credential;
 import androidx.credentials.CredentialManager;
 import androidx.credentials.CredentialManagerCallback;
 import androidx.credentials.CustomCredential;
-import androidx.credentials.GetCredentialException;
 import androidx.credentials.GetCredentialRequest;
+import androidx.credentials.exceptions.GetCredentialException;
 
 import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption;
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential;
